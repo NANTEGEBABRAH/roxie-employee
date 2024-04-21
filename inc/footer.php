8e80cb492622b4f775107502178cb349b42f7755
NANTEGEBABRAH <112953220+NANTEGEBABRAH@users.noreply.github.com>
2024-04-22 00:07:10 +0300

    Add files via upload

---
+++ inc/footer.php
@@ -0,0 +1,16 @@
+<style type="text/css">
+<!--
+.style1 {
+	color: #000000;
+	font-weight: bold;
+}
+.style2 {
+	font-size: 14px;
+	color: #000000;
+}
+-->
+</style>
+ <div class="footer">
+          <div class="style1"> <p align="center" class="style2"> Copyright @ [<?php echo date("Y")?>] | City of faith Employee Management System |Crafted By: Babrah Nantege|AKP/ASC/CSC/HND2021/1488 | All Rights Reserved </p>
+</div>
+</div>
